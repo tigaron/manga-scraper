f14621d9ead7f115acc6be6a05324ecc8503729b
tigaron <suputra@mail.com>
2024-05-25 23:58:26 +0700

    test: change url for local development

--- scripts/seed-series-detail.sh
+++ scripts/seed-series-detail.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# apiUrl="https://manga-scraper.fourleaves.studio"
+apiUrl="http://localhost:1323"
+
 check_error() {
   local response="$1"
   local error=$(echo "$response" | jq -r '.error')
@@ -9,7 +12,7 @@ check_error() {
   fi
 }
 
-providersApi=$(curl -s "https://manga-scraper.fourleaves.studio/api/v1/providers")
+providersApi=$(curl -s "$apiUrl/api/v1/providers")
 check_error "$providersApi"
 
 providers=$(echo "$providersApi" | jq -r '.data[].slug')
@@ -17,13 +20,13 @@ providers=$(echo "$providersApi" | jq -r '.data[].slug')
 for provider in $providers; do
   page=1
   while true; do
-    seriesApi=$(curl -s "https://manga-scraper.fourleaves.studio/api/v1/series/$provider?page=$page&size=10")
+    seriesApi=$(curl -s "$apiUrl/api/v1/series/$provider?page=$page&size=10")
     check_error "$seriesApi"
 
     series=$(echo "$seriesApi" | jq -r '.data[].slug')
     for s in $series; do
       echo "Scraping $s from $provider"
-      curl -s -X PUT "https://manga-scraper.fourleaves.studio/api/v1/scrape-requests/series/detail" \
+      curl -s -X PUT "$apiUrl/api/v1/scrape-requests/series/detail" \
         -H "Content-Type: application/json" \
         -d @- <<EOF
 {
