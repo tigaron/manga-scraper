3956c60e4437c0e2f975a97c785f3cd8906ef3eb
tigaron <suputra@mail.com>
2024-06-05 14:40:48 +0700

    feat: add surya scraper

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -6,12 +6,15 @@ import (
 
 	v1Model "fourleaves.studio/manga-scraper/api/models/v1"
 	"fourleaves.studio/manga-scraper/internal/scraper/asura"
+	"fourleaves.studio/manga-scraper/internal/scraper/surya"
 )
 
 func ScrapeSeriesList(ctx context.Context, browserUrl, provider, url string) ([]v1Model.SeriesList, error) {
 	switch provider {
 	case "asura":
 		return asura.ScrapeSeriesList(ctx, browserUrl, url)
+	case "surya":
+		return surya.ScrapeSeriesList(ctx, browserUrl, url)
 	default:
 		return nil, fmt.Errorf("not implemented yet")
 	}
@@ -21,6 +24,8 @@ func ScrapeSeriesDetail(ctx context.Context, browserUrl, provider, url string) (
 	switch provider {
 	case "asura":
 		return asura.ScrapeSeriesDetail(ctx, browserUrl, url)
+	case "surya":
+		return surya.ScrapeSeriesDetail(ctx, browserUrl, url)
 	default:
 		return v1Model.SeriesDetail{}, fmt.Errorf("not implemented yet")
 	}
@@ -30,6 +35,8 @@ func ScrapeChapterList(ctx context.Context, browserUrl, provider, url string) ([
 	switch provider {
 	case "asura":
 		return asura.ScrapeChapterList(ctx, browserUrl, url)
+	case "surya":
+		return surya.ScrapeChapterList(ctx, browserUrl, url)
 	default:
 		return nil, fmt.Errorf("not implemented yet")
 	}
@@ -39,6 +46,8 @@ func ScrapeChapterDetail(ctx context.Context, browserUrl, provider, url string) 
 	switch provider {
 	case "asura":
 		return asura.ScrapeChapterDetail(ctx, browserUrl, url)
+	case "surya":
+		return surya.ScrapeChapterDetail(ctx, browserUrl, url)
 	default:
 		return v1Model.ChapterDetail{}, fmt.Errorf("not implemented yet")
 	}
