58e8b8b0b71d197e6b03a18b42ff58c73a7020c3
tigaron <suputra@mail.com>
2024-06-21 17:22:24 +0700

    fix: swagger put search param

--- internal/rest/v1/series/series_put_index.go
+++ internal/rest/v1/series/series_put_index.go
@@ -14,6 +14,7 @@ import (
 // @Security		TokenAuth
 // @Tags			series
 // @Produce		json
+// @Param			provider_slug path string true "Provider Slug"
 // @Success		200	{object}	ResponseV1
 // @Failure		401	{object}	ResponseV1
 // @Failure		403	{object}	ResponseV1
